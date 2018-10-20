3e332eab4fd18fd7d667633a0f6d03d28a305945
Jonathan <jon.bluks@gmail.com>
2018-10-20 12:48:08 -0700

    New Build

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.static('public/build'))
 
 app.listen(port, ()=> console.log('BlockGig is Alive!'))
